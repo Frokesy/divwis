425f23956ca36aa5121361568ef2092364f4575a
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-02-04 13:49:07 +0100

    designed NavMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/defaults/Header"
+import NavMenu from "./components/defaults/NavMenu"
 
 function App() {
 
   return (
     <div>
       <Header />
+      <NavMenu />
     </div>
   )
 }
