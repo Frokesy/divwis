d2f6e6cce17e271376a22dfaa5fe19be3a5656f7
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-02-12 23:42:53 +0100

    modified HeroSection for mobile screens

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,16 +1,16 @@
 const HeroSection = () => {
   return (
-    <div className="bg-[url('/assets/hero_img.jpg')] z-0 h-[80vh] bg-center bg-cover bg-no-repeat items-center justify-center">
-      <div className="absolute bg-[#000] bg-opacity-65 text-[#fff] h-[80%] w-[100%]">
-        <div className="flex flex-col justify-center h-[100%] w-[80vw] mx-auto font-mono">
-          <h1 className="text-[60px] font-bold">Looking to buy your</h1>
-          <p className="text-[50px] font-bold">
+    <div className="bg-[url('/assets/hero_img.jpg')] z-0 lg:h-[80vh] h-[70vh] bg-center bg-cover bg-no-repeat items-center justify-center">
+      <div className="absolute bg-[#000] bg-opacity-65 text-[#fff] lg:h-[80%] h-[70%] w-[100%]">
+        <div className="flex flex-col justify-center h-[100%] lg:w-[80vw] w-[95vw] mx-auto lg:font-mono">
+          <h1 className="lg:text-[60px] text-[40px] font-bold">Looking to buy your</h1>
+          <p className="lg:text-[50px] text-[40px] font-bold">
             local stuff at affordable cost?
           </p>
           <p className="text-[18px] pt-4">
             Get your groceries in as fast and easy as you want it.
           </p>
-          <div className="flex w-[40%] space-x-10 mt-10">
+          <div className="flex lg:w-[40%] space-x-10 mt-10">
             <button className="bg-[#19483a] w-[50%] text-[#fff] text-[18px] font-bold px-8 py-2">
               Shop Now
             </button>
