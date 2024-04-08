36a8e1531c450798639fcc967ba19371350922e9
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-04-08 23:47:23 +0100

    modified preloader

--- src/components/wrappers/MainContainer.tsx
+++ src/components/wrappers/MainContainer.tsx
@@ -4,8 +4,6 @@ import NavMenu from "../defaults/NavMenu";
 import PageTransition from "./PageTransition";
 import Footer from "../defaults/Footer";
 import BottomNav from "../defaults/BottomNav";
-import Logo from "../defaults/Logo";
-
 interface MainContainerProps {
   children: ReactNode;
 }
@@ -17,17 +15,17 @@ const MainContainer: FC<MainContainerProps> = ({ children }) => {
   }, 2000);
   return (
     <div>
+      <Header />
+      <NavMenu />
       {loading ? (
         <div>
-          <Header />
-          <NavMenu />
           <PageTransition>{children}</PageTransition>
           <Footer />
           <BottomNav />
         </div>
       ) : (
-        <div className="">
-          <Logo />
+        <div className="flex justify-center items-center min-h-screen">
+          <img src="/assets/preloader.gif" alt="preloader" className="w-[20rem]" />
         </div>
       )}
     </div>
