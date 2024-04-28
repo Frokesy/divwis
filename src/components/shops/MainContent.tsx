29168a6465c63c125c8594043f7a38fe3d69bdbe
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-04-28 22:04:20 +0100

    activated 'shop now' button in shops page

--- src/components/shops/MainContent.tsx
+++ src/components/shops/MainContent.tsx
@@ -17,6 +17,7 @@ interface MainContentProps {
   productsPerPrice: ProductsProps[];
   productsPerCategory: ProductsProps[];
   searchResults: ProductsProps[];
+  handleClick: (product: ProductsProps) => void;
 }
 const MainContent: FC<MainContentProps> = ({
   filterType,
@@ -24,6 +25,7 @@ const MainContent: FC<MainContentProps> = ({
   productsPerPrice,
   productsPerCategory,
   searchResults,
+  handleClick
 }) => {
   const [filteredProducts, setFilteredProducts] =
     useState<ProductsProps[]>(products);
@@ -41,47 +43,52 @@ const MainContent: FC<MainContentProps> = ({
     searchResults,
   ]);
   return (
-    <div className="grid lg:grid-cols-3 grid-cols-1 lg:w-[100%] mt-10 lg:mt-0 mx-auto lg:gap-[1vw] gap-[3vh]">
-      {filteredProducts.map((product) => (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-          }}
-          key={product.id}
-          className="flex flex-col w-[100%] bg-[#fff] rounded-lg"
-        >
-          <div className="relative w-[100%] flex flex-col items-center">
-            <div className="absolute top-0 left-0">
-              <h2 className="bg-[#ff0406] rounded-tl-lg text-[#fff] text-[14px] font-semibold pl-3 pr-6 py-1 uppercase">
-                -12% off
-              </h2>
+    <div>
+      <div className="grid lg:grid-cols-3 grid-cols-1 lg:w-[100%] mt-10 lg:mt-0 mx-auto lg:gap-[1vw] gap-[3vh]">
+        {filteredProducts.map((product) => (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+            key={product.id}
+            className="flex flex-col w-[100%] bg-[#fff] rounded-lg"
+          >
+            <div className="relative w-[100%] flex flex-col items-center">
+              <div className="absolute top-0 left-0">
+                <h2 className="bg-[#ff0406] rounded-tl-lg text-[#fff] text-[14px] font-semibold pl-3 pr-6 py-1 uppercase">
+                  -12% off
+                </h2>
+              </div>
+              <img
+                src={product.productImg}
+                alt="product"
+                className="mt-10 lg:h-[200px] lg:w-[250px] w-[350px] h-[270px]"
+              />
             </div>
-            <img
-              src={product.productImg}
-              alt="product"
-              className="mt-10 lg:h-[200px] lg:w-[250px] w-[350px] h-[270px]"
-            />
-          </div>
-          <div className="px-6">
-            <h3 className="font-bold text-[18px] mt-4">{product.name}</h3>
-            <div className="flex items-center space-x-2 mb-2">
-              <FiveStars size="sm" />
-              <span className="text-[12px] mt-1 text-[#808080]">
-                (5.2k reviews)
+            <div className="px-6">
+              <h3 className="font-bold text-[18px] mt-4">{product.name}</h3>
+              <div className="flex items-center space-x-2 mb-2">
+                <FiveStars size="sm" />
+                <span className="text-[12px] mt-1 text-[#808080]">
+                  (5.2k reviews)
+                </span>
+              </div>
+              <span className="pt-2 text-[#ff0406] font-bold text-[18px]">
+                ${product.price}
               </span>
+              <div
+                onClick={() => handleClick(product)}
+                className="border border-[#ff7c08] text-[#ff7c08] hover:bg-[#ff7c08] transition-colors duration-500 ease-in-out hover:text-[#fff] cursor-pointer my-6 py-3 rounded-lg flex items-center justify-center"
+              >
+                <p className="text-[18px] font-bold">Shop Now</p>
+              </div>
             </div>
-            <span className="pt-2 text-[#ff0406] font-bold text-[18px]">
-              ${product.price}
-            </span>
-            <div className="border border-[#ff7c08] text-[#ff7c08] hover:bg-[#ff7c08] transition-colors duration-500 ease-in-out hover:text-[#fff] cursor-pointer my-6 py-3 rounded-lg flex items-center justify-center">
-              <p className="text-[18px] font-bold">Shop Now</p>
-            </div>
-          </div>
-        </motion.div>
-      ))}
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/shops/index.tsx
+++ src/pages/shops/index.tsx
@@ -3,6 +3,7 @@ import MainContent from "../../components/shops/MainContent";
 import SideNav from "../../components/shops/SideNav";
 import MainContainer from "../../components/wrappers/MainContainer";
 import { useState } from "react";
+import ViewProductModal from "../../components/modals/ViewProductModal";
 
 interface ProductsProps {
   id: number;
@@ -19,9 +20,19 @@ const Shops = () => {
     []
   );
   const [productsPerPrice, setProductsPerPrice] = useState<ProductsProps[]>([]);
-  const [productsPerCategory, setProductsPerCategory] = useState<ProductsProps[]>([]);
+  const [productsPerCategory, setProductsPerCategory] = useState<
+    ProductsProps[]
+  >([]);
   const [searchResults, setSearchResult] = useState<ProductsProps[]>([]);
   const [filterType, setFilterType] = useState<string>("");
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [viewedProduct, setViewedProduct] = useState<ProductsProps>();
+
+  const handleClick = (item: ProductsProps) => {
+    setIsOpen(true);
+    setViewedProduct(item);
+  };
   return (
     <MainContainer active="shop">
       <div className="bg-[#f1f1f1] min-h-[80vh] pb-[10vh] lg:pt-10 pt-4">
@@ -39,6 +50,7 @@ const Shops = () => {
           <div className="main lg:w-[76%]">
             <MainContent
               filterType={filterType}
+              handleClick={handleClick}
               productsPerRating={productsPerRating}
               productsPerPrice={productsPerPrice}
               productsPerCategory={productsPerCategory}
@@ -47,6 +59,13 @@ const Shops = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <ViewProductModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          product={viewedProduct}
+        />
+      )}
     </MainContainer>
   );
 };
