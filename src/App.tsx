76685be010e7f537c380356525bf133f343be9ce
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-04-29 22:51:25 +0100

    installed stripejs

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import Shops from "./pages/shops";
 import { AnimatePresence } from "framer-motion";
 import Account from "./pages/account";
 import Contact from "./pages/contact";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe("");
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/shops/:category_id", element: <Shops /> },
     { path: "/account", element: <Account /> },
-    { path: "/contact", element: <Contact /> }
+    { path: "/contact", element: <Contact /> },
   ]);
+
+  const options = {
+    clientSecret: "{{CLIENT_SECRET}}",
+  };
 
   return (
     <AnimatePresence mode="wait">
-      <RouterProvider router={router} />
+      <Elements stripe={stripePromise} options={options}>
+        <RouterProvider router={router} />
+      </Elements>
     </AnimatePresence>
   );
 }
