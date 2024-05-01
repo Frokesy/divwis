e31a2d992eec1d14a077c58b9fa56539fb112b90
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-01 22:16:54 +0100

    created routes to handle checkout response

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Shops from "./pages/shops";
 import { AnimatePresence } from "framer-motion";
 import Account from "./pages/account";
 import Contact from "./pages/contact";
+import Success from "./pages/res/success";
+import Cancel from "./pages/res/cancel";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/shops/:category_id", element: <Shops /> },
     { path: "/account", element: <Account /> },
-    { path: "/contact", element: <Contact /> }
+    { path: "/contact", element: <Contact /> },
+    { path: "/success", element: <Success /> },
+    { path: "/cancel", element: <Cancel /> }
   ]);
 
   return (
