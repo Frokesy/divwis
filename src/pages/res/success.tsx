cae5f812bf0735c8709a9be39995d8372db23163
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-04 22:05:52 +0100

    modified success page

--- src/pages/res/success.tsx
+++ src/pages/res/success.tsx
@@ -1,8 +1,8 @@
 const Success = () => {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center justify-center h-screen">
             <img src="/assets/success.gif" alt="success" />
-            <h2>Payment Successful, Order details have been forwarded to your email!</h2>
+            <h2 className="text-center text-[#808080] font-semibold mt-10">Payment Successful, Order details have been forwarded to your email!</h2>
         </div>
     )
 }
