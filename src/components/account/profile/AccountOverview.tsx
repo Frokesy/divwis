e9983633d622e699e2ba7e4c04201540bb1d74c3
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-06-18 17:40:52 +0100

    added spinner on accounts page to improve user feedback

--- src/components/account/profile/AccountOverview.tsx
+++ src/components/account/profile/AccountOverview.tsx
@@ -3,6 +3,7 @@ import React, { FC, useState } from "react";
 import { FaEdit, FaPhoneAlt, FaUser } from "react-icons/fa";
 import { FaLocationPin } from "react-icons/fa6";
 import EditAddress from "../address/EditAddress";
+import Spinner from "../../defaults/Spinner";
 
 interface ProfileProps {
   editStatus: boolean;
@@ -42,123 +43,131 @@ const AccountOverview: FC<ProfileProps> = ({
         />
       ) : (
         <div>
-          {userData.map((user) => (
-            <div key={user.id}>
-              <div className="">
-                <div className="flex lg:items-center space-x-6 pb-10">
-                  <h2 className="lg:text-[26px] text-[18px] font-bold font-mono lg:text-[#808080]">
-                    Personal Information
-                  </h2>
-                  <FaEdit
-                    fill="#6eb356"
-                    className="mt-0.5 block lg:hidden"
-                    onClick={() => setEditStatus(!editStatus)}
-                  />
-                  <p
-                    className="text-[#6eb356] text-[15px] font-semibold cursor-pointer lg:block hidden"
-                    onClick={() => setEditStatus(!editStatus)}
-                  >
-                    Edit
-                  </p>
-                </div>
-                <div className="flex lg:items-center space-y-6 lg:space-y-0 lg:flex-row flex-col lg:space-x-6">
-                  <div className="text-[#ccc] bg-[#f1f1f1] w-[8rem] flex items-center justify-center py-6 rounded-full">
-                    <FaUser size={80} />
-                  </div>
-                  <div className="space-y-1">
-                    <h2 className="lg:text-[24px] text-[18px] mb-3 font-semibold">
-                      {user.name}
-                    </h2>
-                    <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
-                      <FaLocationPin />
-                      <p>Mayfair, Ile-Ife. Osun State.</p>
-                    </div>
-                    <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
-                      <FaPhoneAlt />
-                      <p>{user.phone ? user.phone : "null"}</p>
-                    </div>
-                    <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
-                      <FaLocationPin />
-                      <p>{user.email}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-20 lg:w-[80%] w-[100%]">
-                <div className="flex justify-between items-center mb-4">
-                  <h2 className="lg:text-[26px] text-[20px] font-bold font-mono lg:text-[#808080] pb-2">
-                    Address Book
-                  </h2>
-                  <button
-                    onClick={() => setEditAddress(true)}
-                    className="bg-[#6eb356] text-[#fff] h-[40px] w-[200px] text-[14px] font-semibold rounded-md"
-                  >
-                    Add New Address
-                  </button>
-                </div>
-                <div className="flex justify-between lg:flex-row flex-col lg:space-y-0 space-y-6">
+          {userData.length > 0 ? (
+            <div>
+              {userData.map((user) => (
+                <div key={user.id}>
                   <div className="">
-                    <h2 className="font-mono text-[#808080] uppercase font-semibold">
-                      default delivery address
-                    </h2>
-                    <p className="font-semibold mt-2">FrokesLini Noah</p>
-                    <p className="text-[#404040] text-[14px]">
-                      Mayfair, Ile-Ife. Osun State.
-                    </p>
-                    <p className="text-[#404040] text-[14px]">
-                      (+234) 9157881431
-                    </p>
+                    <div className="flex lg:items-center space-x-6 pb-10">
+                      <h2 className="lg:text-[26px] text-[18px] font-bold font-mono lg:text-[#808080]">
+                        Personal Information
+                      </h2>
+                      <FaEdit
+                        fill="#6eb356"
+                        className="mt-0.5 block lg:hidden"
+                        onClick={() => setEditStatus(!editStatus)}
+                      />
+                      <p
+                        className="text-[#6eb356] text-[15px] font-semibold cursor-pointer lg:block hidden"
+                        onClick={() => setEditStatus(!editStatus)}
+                      >
+                        Edit
+                      </p>
+                    </div>
+                    <div className="flex lg:items-center space-y-6 lg:space-y-0 lg:flex-row flex-col lg:space-x-6">
+                      <div className="text-[#ccc] bg-[#f1f1f1] w-[8rem] flex items-center justify-center py-6 rounded-full">
+                        <FaUser size={80} />
+                      </div>
+                      <div className="space-y-1">
+                        <h2 className="lg:text-[24px] text-[18px] mb-3 font-semibold">
+                          {user.name}
+                        </h2>
+                        <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
+                          <FaLocationPin />
+                          <p>Mayfair, Ile-Ife. Osun State.</p>
+                        </div>
+                        <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
+                          <FaPhoneAlt />
+                          <p>{user.phone ? user.phone : "null"}</p>
+                        </div>
+                        <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
+                          <FaLocationPin />
+                          <p>{user.email}</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="">
-                    <h2 className="font-mono text-[#808080] uppercase font-semibold">
-                      current delivery address
+
+                  <div className="mt-20 lg:w-[80%] w-[100%]">
+                    <div className="flex justify-between items-center mb-4">
+                      <h2 className="lg:text-[26px] text-[20px] font-bold font-mono lg:text-[#808080] pb-2">
+                        Address Book
+                      </h2>
+                      <button
+                        onClick={() => setEditAddress(true)}
+                        className="bg-[#6eb356] text-[#fff] h-[40px] w-[200px] text-[14px] font-semibold rounded-md"
+                      >
+                        Add New Address
+                      </button>
+                    </div>
+                    <div className="flex justify-between lg:flex-row flex-col lg:space-y-0 space-y-6">
+                      <div className="">
+                        <h2 className="font-mono text-[#808080] uppercase font-semibold">
+                          default delivery address
+                        </h2>
+                        <p className="font-semibold mt-2">FrokesLini Noah</p>
+                        <p className="text-[#404040] text-[14px]">
+                          Mayfair, Ile-Ife. Osun State.
+                        </p>
+                        <p className="text-[#404040] text-[14px]">
+                          (+234) 9157881431
+                        </p>
+                      </div>
+                      <div className="">
+                        <h2 className="font-mono text-[#808080] uppercase font-semibold">
+                          current delivery address
+                        </h2>
+                        <p className="font-semibold mt-2">FrokesLini Noah</p>
+                        <p className="text-[#404040] text-[14px]">
+                          Mayfair, Ile-Ife. Osun State.
+                        </p>
+                        <p className="text-[#404040] text-[14px]">
+                          (+234) 9157881431
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="my-20">
+                    <h2 className="text-[26px] font-bold font-mono text-[#808080] pb-2">
+                      Account Summary
                     </h2>
-                    <p className="font-semibold mt-2">FrokesLini Noah</p>
-                    <p className="text-[#404040] text-[14px]">
-                      Mayfair, Ile-Ife. Osun State.
-                    </p>
-                    <p className="text-[#404040] text-[14px]">
-                      (+234) 9157881431
-                    </p>
+
+                    <div className="grid lg:grid-cols-4 grid-cols-2 gap-y-10 lg:pr-6 mx-auto">
+                      <div className="flex flex-col items-center">
+                        <span className="text-[34px] font-bold text-[#21b169]">
+                          10
+                        </span>
+                        <p className="text-[14px]">Total Orders Completed</p>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <span className="text-[34px] font-bold text-[#fcb221]">
+                          3
+                        </span>
+                        <p className="text-[14px]">Pending Orders</p>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <span className="text-[34px] font-bold text-[#44c3f7]">
+                          7
+                        </span>
+                        <p className="text-[14px]">Delivered Orders</p>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <span className="text-[34px] font-bold text-[#a158ff]">
+                          $2k+
+                        </span>
+                        <p className="text-[14px]">Total Amount Spent</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="my-20">
-                <h2 className="text-[26px] font-bold font-mono text-[#808080] pb-2">
-                  Account Summary
-                </h2>
-
-                <div className="grid lg:grid-cols-4 grid-cols-2 gap-y-10 lg:pr-6 mx-auto">
-                  <div className="flex flex-col items-center">
-                    <span className="text-[34px] font-bold text-[#21b169]">
-                      10
-                    </span>
-                    <p className="text-[14px]">Total Orders Completed</p>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <span className="text-[34px] font-bold text-[#fcb221]">
-                      3
-                    </span>
-                    <p className="text-[14px]">Pending Orders</p>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <span className="text-[34px] font-bold text-[#44c3f7]">
-                      7
-                    </span>
-                    <p className="text-[14px]">Delivered Orders</p>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <span className="text-[34px] font-bold text-[#a158ff]">
-                      $2k+
-                    </span>
-                    <p className="text-[14px]">Total Amount Spent</p>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
+          ) : (
+            <div className="h-[70vh] flex items-center justify-center">
+              <Spinner />
+            </div>
+          )}
         </div>
       )}
     </motion.div>
