e1b725906b6d6fc0d60b621d67de1363bf9ebc83
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-06-18 22:44:13 +0100

    fetched address from backend

--- src/components/account/profile/AccountOverview.tsx
+++ src/components/account/profile/AccountOverview.tsx
@@ -1,9 +1,10 @@
 import { motion } from "framer-motion";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { FaEdit, FaPhoneAlt, FaUser } from "react-icons/fa";
 import { FaLocationPin } from "react-icons/fa6";
 import EditAddress from "../address/EditAddress";
 import Spinner from "../../defaults/Spinner";
+import { supabase } from "../../../../utils/supabaseClient";
 
 interface ProfileProps {
   editStatus: boolean;
@@ -19,12 +20,39 @@ interface UserProps {
   phone: string;
 }
 
+interface AddressProps {
+  city: string;
+  created_at: string;
+  default: boolean;
+  deliveryAddress: string;
+  id: number;
+  name: string;
+  mobileNumber: string;
+  region: string;
+  userId: string;
+}
+
 const AccountOverview: FC<ProfileProps> = ({
   editStatus,
   setEditStatus,
   userData,
 }) => {
   const [editAddress, setEditAddress] = useState<boolean>(false);
+  const [addresses, setAddresses] = useState<AddressProps[]>([]);
+
+  const fetchAddresses = async () => {
+    const { data: address, error } = await supabase.from("address").select("*");
+    if (error) {
+      console.log(error);
+      return [];
+    }
+    setAddresses(address as AddressProps[]);
+  };
+
+  const defaultAddress = addresses.filter((address) => address.default);
+  useEffect(() => {
+    fetchAddresses();
+  }, []);
 
   return (
     <motion.div
@@ -74,7 +102,7 @@ const AccountOverview: FC<ProfileProps> = ({
                         </h2>
                         <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
                           <FaLocationPin />
-                          <p>Mayfair, Ile-Ife. Osun State.</p>
+                          <p>{defaultAddress?.[0].deliveryAddress}</p>
                         </div>
                         <div className="flex items-center text-[14px] space-x-2 text-[#404040]">
                           <FaPhoneAlt />
@@ -89,41 +117,33 @@ const AccountOverview: FC<ProfileProps> = ({
                   </div>
 
                   <div className="mt-20 lg:w-[80%] w-[100%]">
-                    <div className="flex justify-between items-center mb-4">
-                      <h2 className="lg:text-[26px] text-[20px] font-bold font-mono lg:text-[#808080] pb-2">
+                    <div className="flex justify-between items-center lg:mb-4 mb-6">
+                      <h2 className="lg:text-[26px] text-[20px] font-bold font-mono lg:text-[#808080]">
                         Address Book
                       </h2>
                       <button
                         onClick={() => setEditAddress(true)}
-                        className="bg-[#6eb356] text-[#fff] h-[40px] w-[200px] text-[14px] font-semibold rounded-md"
+                        className="bg-[#6eb356] text-[#fff] h-[40px] lg:w-[200px] w-[140px] lg:text-[14px] text-[13px] font-semibold rounded-md"
                       >
                         Add New Address
                       </button>
                     </div>
-                    <div className="flex justify-between lg:flex-row flex-col lg:space-y-0 space-y-6">
-                      <div className="">
-                        <h2 className="font-mono text-[#808080] uppercase font-semibold">
-                          default delivery address
-                        </h2>
-                        <p className="font-semibold mt-2">FrokesLini Noah</p>
-                        <p className="text-[#404040] text-[14px]">
-                          Mayfair, Ile-Ife. Osun State.
-                        </p>
-                        <p className="text-[#404040] text-[14px]">
-                          (+234) 9157881431
-                        </p>
-                      </div>
-                      <div className="">
-                        <h2 className="font-mono text-[#808080] uppercase font-semibold">
-                          current delivery address
-                        </h2>
-                        <p className="font-semibold mt-2">FrokesLini Noah</p>
-                        <p className="text-[#404040] text-[14px]">
-                          Mayfair, Ile-Ife. Osun State.
-                        </p>
-                        <p className="text-[#404040] text-[14px]">
-                          (+234) 9157881431
-                        </p>
+                    <div className="lg:space-y-0 space-y-6">
+                      <h2 className="font-mono text-[#808080] uppercase font-semibold">
+                        delivery addresses
+                      </h2>
+                      <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-[10vw] gap-[5vh]">
+                        {addresses.map((address) => (
+                          <div key={address.id} className="space-y-2">
+                            <p className="font-semibold mt-2">{address.name}</p>
+                            <p className="text-[#404040] text-[13px]">
+                              {`${address.deliveryAddress}, ${address.region}. ${address.city}`}
+                            </p>
+                            <p className="text-[#404040] text-[14px]">
+                              {address.mobileNumber}
+                            </p>
+                          </div>
+                        ))}
                       </div>
                     </div>
                   </div>
