8713778ce144fb901cdbbc4fc7b36589301e0770
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-03 23:32:41 +0100

    wrote logic for private route

---
+++ src/components/defaults/PrivateRoute.tsx
@@ -0,0 +1,48 @@
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import { supabase } from "../../../utils/supabaseClient";
+
+interface PrivateRouteProps {
+  children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token =
+        localStorage.getItem("authToken") ||
+        sessionStorage.getItem("authToken");
+
+      if (token) {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+
+        if (user) {
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      } else {
+        setAuthenticated(false)
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (authenticated === null) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center">
+        <Spinner color="#000" />
+      </div>
+    );
+  }
+
+  return authenticated ? <>{children}</> : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
